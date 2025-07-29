c01cf37af2a040862a959b5d0f98de0addeb790a
Prg-gurjar <157681075+Prg-gurjar@users.noreply.github.com>
2025-07-29 15:58:41 +0530

    Update WebConfig.java

--- adtech-backend/src/main/java/com/adtech/reportingsystem/config/WebConfig.java
+++ adtech-backend/src/main/java/com/adtech/reportingsystem/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins(allowedOrigins) // Use the injected value
+                .allowedOrigins(.allowedOrigins("http://localhost:3000", "https://adtech-reporting-system-n1w9.vercel.app/")) // Use the injected value
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
